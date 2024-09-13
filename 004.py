3c62f8d9ab05bb1d08729b2ffafbe9654950a18a
Deepak kumar <deepak@kali.Deepakk>
2024-09-13 09:13:05 -0500

    Adding the python file

---
+++ 004.py
@@ -0,0 +1,13 @@
+# write a program to store seven fruits name enter by the users 
+fruit=[]
+f1=input("Name of fruits: ")
+fruit.append(f1)
+f2=input("Name of fruits: ")
+fruit.append(f2)
+f3=input("Name of fruits: ")
+fruit.append(f3)
+f4=input("Name of fruits: ")
+fruit.append(f4)
+
+print(fruit)
+
